0547dcdf4026a593c94f9a2648975933c4309b09
Maquina  de Vendas <mac@MacBook-Air-de-mac.local>
2026-06-27 20:47:43 -0300

    chore: remove dangerous temp-query and temporary scripts

--- test_invoke.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(
-  'https://ilcvydgogqumwjrpzzro.supabase.co',
-  'sb_publishable_njteejatxOX3GqJpiNffpg_Wiq95Umu'
-);
-
-// We need an active session to call the edge function (it requires auth). 
-// Since we don't have the user's password, we can't test it directly unless we bypass auth or have a token.
